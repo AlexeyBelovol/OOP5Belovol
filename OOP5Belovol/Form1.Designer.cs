6e21e6d1a470d7994005cddc68c1928629ad5e35
AlexeyBelovol <belvl.alexey@gmail.com>
2024-04-10 17:35:52 +0300

    Done!

--- OOP5Belovol/Form1.Designer.cs
+++ OOP5Belovol/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxVar1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxVar2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxVar3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(190, 229);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(168, 105);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Виконати дії";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Перша змінна:";
+            // 
+            // textBoxVar1
+            // 
+            this.textBoxVar1.Location = new System.Drawing.Point(190, 98);
+            this.textBoxVar1.Name = "textBoxVar1";
+            this.textBoxVar1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVar1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Результат - ";
+            // 
+            // textBoxVar2
+            // 
+            this.textBoxVar2.Location = new System.Drawing.Point(394, 101);
+            this.textBoxVar2.Name = "textBoxVar2";
+            this.textBoxVar2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVar2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(307, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Друга змінна:";
+            // 
+            // textBoxVar3
+            // 
+            this.textBoxVar3.Location = new System.Drawing.Point(607, 104);
+            this.textBoxVar3.Name = "textBoxVar3";
+            this.textBoxVar3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVar3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(520, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Третя змінна:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(520, 275);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxVar3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxVar2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxVar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота №5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxVar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxVar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxVar3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
--- OOP5Belovol/Form1.cs
+++ OOP5Belovol/Form1.cs
@@ -16,5 +16,22 @@ namespace OOP5Belovol
         {
             InitializeComponent();
         }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            string var1 = textBoxVar1.Text;
+            string var2 = textBoxVar2.Text;
+            string var3 = textBoxVar3.Text;
+
+            if (!char.IsDigit(var1[0]) || !char.IsDigit(var2[0]) || !char.IsDigit(var3[0]))
+            {
+                labelResult.Text = "Неможливо утворити число з символьних змінних.";
+            }
+            else
+            {
+                int number = int.Parse(var1 + var2 + var3);
+                labelResult.Text = "Утворене число: " + number;
+            }
+        }
     }
 }
